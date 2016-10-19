1027fb61815715e445e91af7da613939eec5d56b
mcasciato <michael.casciato@icloud.com>
2016-10-19 17:15:29 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,17 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter the favorite food correctly', function(){
+		var foods = [
+			{name: 'Michael', favoriteFood: 'spaghetti'},
+			{name: 'Tony', favoriteFood: 'meatballs'},
+			{name: 'Matteo', favoriteFood: 'lasagna'}
+		]
+
+		var result = $filter('favoriteFood')(foods, 'lasagna')
+
+		expect(result.length).toBe(1);
+		expect(result[0].name).toBe('Matteo')
+	});
+
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,12 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should remove all of the vowels from a string', function () {
+		var string = "Well what do we have here!?"
+
+		result = $filter('removeAllVowels')(string)
+
+		expect(result).toEqual('Wll wht d w hv hr!?')
+	});
+
 });
